3c52ae908809f9bd300f621616de8a5bcc481b2c
Misboonyos <boonyos.k@gmail.com>
2025-11-06 23:43:24 +0700

    props, destructuring, children prop, .map method

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import Title from './components/Title'
 import Book from './components/Book'
 
 const App = () => {
+  const user = [
+    {id: 1, name: "Jan"},
+    {id: 2, name: "Feb"}
+  ]
+
   return (
     <div>
       <Title 
-      txt="Easy React"
-      price={500} />
-      <Book />
+        txt="Easy React"         price={500}       />
+      <Book data={user}>
+        <p>
+          This is a book about React.
+          <span> It is very useful for beginners. </span>
+        </p>
+        <button>Add to Cart</button>
+      </Book>              
       </div>
   )
 }
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,9 +1,21 @@
-// rafce
+//rafce
 import React from 'react'
 
-const Book = () => {
+const Book = ({data, children}) => {
+  //JSX
+  console.log(data)
+ 
   return (
-    <div>Book</div>
+    <div> 
+     {data.map((el, i)=> {
+        // function body
+        return <h1 key={i}>{el.name}</h1>
+        // webpage result: Jan Feb
+        // console result: {id: 1, name: "Jan"}, {id: 2, name: "Feb"}
+      } )
+     }
+    </div>
+   
   )
 }
 
