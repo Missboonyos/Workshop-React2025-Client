52d9a521266eeb3e866d922dddfc1c93ae91885f
Misboonyos <boonyos.k@gmail.com>
2025-11-10 22:53:21 +0700

    ep02 JS Array .map()-still pending

--- src/components/ListBasic.jsx
+++ src/components/ListBasic.jsx
@@ -8,13 +8,23 @@ const ListBasic = () => {
         {id: 2, name: 'Bob'},
         {id: 3, name: 'Charlie'}
     ];
-
-        console.log(users[0].name);// Alice
-        console.log(users[1].name);// Bob
-        console.log(users[2].name);// Charlie
-        console.log(users[1]);// {id: 2, name: 'Bob'}
+        // console.log(users[0].name);// Alice
+        // console.log(users[1].name);// Bob
+        // console.log(users[2].name);// Charlie
+        // console.log(users[1]);// {id: 2, name: 'Bob'}
+        //.map(fn (el,index, arr))
   return (
-    <div>ListBasic</div>
+    <div>ListBasic
+        {
+            users.map ((el)=> {
+                // fn body
+                return <div key={el.id}>
+                <h1>{el.name}</h1>
+                <h2>{el.id}</h2>
+                </div>                
+            })
+        }
+    </div>
   )
 }
 
