80b59f7327aed434c762253c4f3a2ee414e50858
Misboonyos <boonyos.k@gmail.com>
2025-11-11 17:54:52 +0700

    EP02:array method .map(), props--done

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import FormInput from './components/FormInput'
 import ListBasic from './components/ListBasic'
 
 const App = () => {
+  const users = [
+        {id: 1, name: 'Alice'},
+        {id: 2, name: 'Bob'},
+        {id: 3, name: 'Charlie'}
+    ];   
+
   return (
     <>
     <FormInput />
-    <ListBasic />
+    <ListBasic users={users}/>
     </>
   )
 }
--- src/components/ListBasic.jsx
+++ src/components/ListBasic.jsx
@@ -1,31 +1,17 @@
 //rafce
 import React from 'react'
 
-const ListBasic = () => {
-
-    const users = [
-        {id: 1, name: 'Alice'},
-        {id: 2, name: 'Bob'},
-        {id: 3, name: 'Charlie'}
-    ];
-        // console.log(users[0].name);// Alice
-        // console.log(users[1].name);// Bob
-        // console.log(users[2].name);// Charlie
-        // console.log(users[1]);// {id: 2, name: 'Bob'}
-        //.map(fn (el,index, arr))
-  return (
-    <div>ListBasic
-        {
-            users.map ((el)=> {
-                // fn body
-                return <div key={el.id}>
-                <h1>{el.name}</h1>
-                <h2>{el.id}</h2>
-                </div>                
-            })
-        }
-    </div>
-  )
+const ListBasic = (props) => {
+    const {users} = props; //destructuring      
+    console.log(users)
+    return <div>
+        ListBasic 
+        {users.map((el,i)=>{
+            // return <h1 key={i}>{el.name}</h1>;              
+            return <h1 key={i}>{el.name}</h1>;              
+    })
 }
-
+    </div>    
+    
+}
 export default ListBasic
